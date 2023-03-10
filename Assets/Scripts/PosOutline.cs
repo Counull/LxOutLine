371741b463336db7d35cc1ede3d4dea8228e97f9
liuh <counull@163.com>
2023-03-11 01:36:48 +0800

    outline pos

---
+++ Assets/Scripts/PosOutline.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PosOutline : MonoBehaviour {
+    public Color outlineColor;
+
+    [FormerlySerializedAs("outline")] [Range(0.0f, 1.0f)]
+    public float outlineWidth = 0.0f;
+
+    private Material _mat;
+    private static readonly int OutLineColor = Shader.PropertyToID("_OutLineColor");
+    private static readonly int OutLineWidth = Shader.PropertyToID("_OutLineWidth");
+
+    private void Awake() {
+        _mat = GetComponent<MeshRenderer>().material;
+    }
+
+
+    private void Update() {
+   
+        _mat.SetColor(OutLineColor, outlineColor);
+        _mat.SetFloat(OutLineWidth, outlineWidth);
+    }
+}
